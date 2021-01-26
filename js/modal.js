85562a38e21141e0444255801d404cc21d18eff4
Sergiy Yuschenko <eester33@gmail.com>
2021-01-26 16:12:15 +0200

    Оформлює стилі бекдропу для адаптивного сайту

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
+      document.body.classList.toggle("modal-open"); //усуває скрол по сторінці коли відкрита модалка
       refs.modal.classList.toggle('is-hidden');
     }
   })();
